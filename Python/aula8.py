12941561bf6795b6e9cd3492ea2271d37033a9d9
Luiz <lcmoura@yahoo.com>
2026-07-22 09:33:15 -0300

    Atualizando Linux

---
+++ Python/aula8.py
@@ -0,0 +1,40 @@
+import sys
+INVERT = "\x1b[1;7m"
+RESET = "\x1b[0m"
+GREEN = "\x1b[1;32m"
+YELLOW = "\x1b[1;34;43m"
+
+K = "Oi, "
+n1 = None
+DATA = ["mundo", "Ana", "Bela"]
+READ = iter(DATA)
+
+def saudar():
+    print(f"n1 = {n1}")      
+    print(f"{K}{n1}")        
+
+print(f"{GREEN}LITERAL, CONSTANTE e VARIAVEL{RESET}")
+
+print(f"\nLiteral classico helloworld: '{INVERT}Oi, mundo!{RESET}'")
+
+print("\nValor da variavel antes da inicializacao: ")
+saudar()
+
+print(f"\n{YELLOW}Atribuicoes em design-time{RESET}")
+n1 = "Ana"
+print("Inicializacao direta: ")
+saudar()
+
+n1 = next(READ);    n1 = next(READ);    n1 = next(READ)
+print("\nNova atribuicao simulando read/data: ")
+saudar()
+
+print(f"\n{YELLOW}Atribuicao em invocation-time{RESET}")    # python argv[0] argv[1]
+n1 = sys.argv[1] if len(sys.argv) > 1 else ""
+print("Entrada por linha de comando: ")
+saudar()
+
+print(f"\n{YELLOW}Atribuicao em runtime{RESET}")      
+print("Entrada por interacao: ")
+n1 = input("Digite um nome: ")                   
+saudar()
