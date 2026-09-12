408fbc0451ce56d11e9112524e78f86fce95b356
luizmoura1 <lcmoura@yahoo.com>
2026-09-12 20:53:07 -0300

    Introdução a Perl

--- Perl/aula1.pl
+++ Perl/aula1.pl
@@ -1,5 +1,5 @@
 # comentário em linha inteira
-$SAUDACAO="Oi, ";  # comentário em fim de linha   ;sigil scale ; simples, um único valor          
+$SAUDACAO="Oi, ";  # comentário em fim de linha; scale;     sigil; valor único, simples        
 $nome;                     
 
 $GREEN="\e[32m";
@@ -7,27 +7,20 @@ $RESET="\e[0m";
 $YELLOW="\e[32;43;1;3m";
 $INVERT="\e[7m";
 
-print "${GREEN}LITERAL, CONSTANTE, VARIAVEL$RESET";
-print "\nLiteral clássico helloworld:\t";
-print $INVERT, "Oi, mundo!", 
-$RESET;
+print "${GREEN}LITERAL, CONSTANTE, VARIAVEL${RESET}";
+print "\n\nLiteral clássico helloworld:\t${INVERT}Oi, mundo!${RESET}";
 
-print "\nValor da variável antes da inicialização:\n";   
-print "nome: '$nome'\n";
-print "$SAUDACAO$nome\n";
+print "\n\nValor da variável antes da inicialização:\n";
+print "nome: '${nome}'\n";
+print "${SAUDACAO}${nome}!\n";
 
-print "\n${YELLOW}ATRIBUICOES EM DESIGN-TIME${RESET}\n";
-print "Inicialização:\n";
+print "\n${YELLOW}ATRIBUICOES EM DESIGN-TIME${RESET}";
+print "\nInicialização:\n";
 $nome="Ana";
-print "nome: '$nome'\n";
-print "$SAUDACAO$nome\n";
+print "nome: '${nome}'\n";
+print "${SAUDACAO}${nome}!\n";
 
-print "\nNova 
-atribuição:";
-print   "\n"    ;
+print "\nNova atribuição:\n";
 $nome="Bela";
-print 
-"nome: '$nome'\n" ;
-print 
-"$SAUDACAO$nome\n"
-;
+print "nome: '${nome}'\n";
+print "${SAUDACAO}${nome}!\n";
